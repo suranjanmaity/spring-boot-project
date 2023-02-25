0785b20bb4a0a99f6e138bd7872a13e5476a1b6d
suranjanmaity <suranjanmaity4@gmail.com>
2023-02-25 21:38:11 +0530

    initial commit

---
+++ store-maangement/src/main/java/com/jsk/store_management/StoreMaangementApplication.java
@@ -0,0 +1,13 @@
+package com.jsk.store_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreMaangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreMaangementApplication.class, args);
+	}
+
+}
---
+++ store-maangement/src/test/java/com/jsk/store_management/StoreMaangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsk.store_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreMaangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
